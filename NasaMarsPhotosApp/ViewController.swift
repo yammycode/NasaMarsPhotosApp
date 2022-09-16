a18653f8d4e63277539bbc21ad854719a2ce5c5c
Anton <info@yammycode.com>
2022-09-16 21:09:40 +0300

    Initial Commit

---
+++ NasaMarsPhotosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NasaMarsPhotosApp
+//
+//  Created by Anton Saltykov on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
